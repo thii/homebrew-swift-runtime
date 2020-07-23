d21cb638f6adfe53f757bb460cf3d7428831e226
toobuntu <toobuntu@gmail.com>
2020-07-24 00:27:07 +0200

    Update swift-runtime.rb
    Update to current version and add uninstall field

--- Casks/swift-runtime.rb
+++ Casks/swift-runtime.rb
@@ -1,10 +1,12 @@
 cask 'swift-runtime' do
-  version :latest
+  version '061-41823-20191025-5efc5a59-d7dc-46d3-9096-396bb8cb4a73'
   sha256 :no_check
 
-  url 'https://updates.cdn-apple.com/2019/macos/041-36337-20190325-11FD143F-6569-4A78-8ABF-157D16A516F1/SwiftRuntimeForCommandLineTools.dmg'
+  url "https://updates.cdn-apple.com/2019/cert/#{version}/SwiftRuntimeForCommandLineTools.dmg"
   name 'Swift Runtime Support For Command Line Tools'
   homepage 'https://support.apple.com/kb/DL1998'
 
   pkg 'SwiftRuntimeForCommandLineTools.pkg'
+  
+  uninstall pkgutil: 'com.apple.pkg.SwiftRuntimeForCommandLineTools'
 end
