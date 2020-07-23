dc232ec43fda452f1b462cb78fdcd68a4e2590ae
toobuntu <toobuntu@gmail.com>
2020-07-24 01:12:33 +0200

    Update swift-runtime.rb
    Add checksum

--- Casks/swift-runtime.rb
+++ Casks/swift-runtime.rb
@@ -1,6 +1,6 @@
 cask 'swift-runtime' do
   version '061-41823-20191025-5efc5a59-d7dc-46d3-9096-396bb8cb4a73'
-  sha256 :no_check
+  sha256 'd5956aa3c543cdb6e0031f63c3d6a04b149e341c2dccfe917e794912015650ca'
 
   url "https://updates.cdn-apple.com/2019/cert/#{version}/SwiftRuntimeForCommandLineTools.dmg"
   name 'Swift Runtime Support For Command Line Tools'
